61a4559ce68225978bbc1b7120e3ad0846d3e2d5
Aska-codeprogramer <askaalghifari487@gmail.com>
2025-06-26 12:22:15 +0700

    Add files via upload

---
+++ interaktif.js
@@ -0,0 +1,16 @@
+function linkig() {
+    alert("mau liat instagram ku...?");
+    alert("jangan di sebar ya....");
+    console.log("click logo instagram");
+}
+
+function linktt() {
+    alert("mau liat tiktok ku...?");
+    alert("jangan di sebar tiktok ku yaa...");
+    console.log("click logo tiktok");
+}
+
+function hubungi() {
+    alert("upps, ini belum berfungsi");
+    console.log("belum berfungsi");
+}
